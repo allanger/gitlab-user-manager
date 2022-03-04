0314a2d61b1643fc2d524186c6ef4b79e5e155f1
Allen Languor <allanger@zohomail.com>
2022-03-04 13:04:58 +0100

    script is ready

--- scripts/download_gum.sh
+++ scripts/download_gum.sh
@@ -34,4 +34,16 @@ case "$(uname)" in
   ;;
 esac
 LATEST_VERSION=$(curl https://allanger.github.io/gitlab-user-manager/)
-curl "https://github.com/allanger/gitlab-user-manager/releases/download/$LATEST_VERSION/gum-$LATEST_VERSION-$TARGET"
+echo "Downloading"
+RELEASE_NAME=gum-$LATEST_VERSION-$TARGET
+RELEASE_URL="https://github.com/allanger/gitlab-user-manager/releases/download/$LATEST_VERSION/$RELEASE_NAME"
+echo $RELEASE_URL
+curl -LJO $RELEASE_URL
+
+mv $RELEASE_NAME gum
+chmod +x gum
+
+echo "Make sure that gum is in your $PATH"
+echo 'Try: '
+echo ' $ export PATH=$PATH:$PWD'
+echo ' $ gum -h'
